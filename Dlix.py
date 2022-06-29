48d4d173ccbf0d6be32f86da44c3e1b69c48ab9b
AdliXSec <naufalsyahruradli@gmail.com>
2022-06-30 00:27:43 +0700

    Telegram BOT by AdliXSec (update 30 juni 2022)

--- Dlix.py
+++ Dlix.py
@@ -1,11 +1,29 @@
 from email import message
+from click import command
 from telebot import *
 import socket
-import json
+import json, os
+import pafy
+from urllib.request import urlopen
 import re
 import requests
 import random
 import hashlib
+from bs4 import BeautifulSoup as bes
+
+# pip install pytelegrambotapi
+# pip install youtube_dl
+# pip install requests
+# pip install beautifulsoup4
+
+# Note : edit file backend_youtube_dl.py in 
+# line 53 and 54, 
+# change to self._likes = self._ydl_info.get('like_count',0) 
+# And self._dislikes = self._ydl_info.get('dislike_count',0)
+        
+
+id = 861422650
+username_owner = "itsmeAdli"
 
 api = "Token API"
 bot = TeleBot(api)
@@ -13,38 +31,25 @@ bot = TeleBot(api)
 sapa = ['Hai Juga', 'Hallo Juga', 'Hi Juga', 'Aloo Juga']
 nama = ['Nama Saya adalah DliX Bot\nYang di ciptakan oleh Tuan Adli ']
 
-
-# Bagian Menu
-
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
-    bot.reply_to(message, 'Hi, apa kabar {} {}? Ada yang bisa saya bantu?'.format(first_name,last_name))
+    if message.from_user.id == id and message.from_user.username == username_owner:
+        bot.reply_to(message, 'Hallo, senang bertemu anda kembali. Tuan, apakah ada yang bisa saya bantu?')
+    else:
+        bot.reply_to(message, 'Hi, apa kabar {} {}? Ada yang bisa saya bantu? Jika ada ketik \n/help'.format(first_name,last_name))
     markup = types.ReplyKeyboardMarkup()
-    item3 = types.KeyboardButton('/menu')
-    item4 = types.KeyboardButton('/bantuan')
+    item4 = types.KeyboardButton('/help')
     item5 = types.KeyboardButton('/cek_id')
-    markup.row(item3, item4)
-    markup.row(item5)
-    chatid = message.chat.id
-    bot.send_message(chatid, 'Silahkan pilih', reply_markup=markup)
-    
-@bot.message_handler(commands=['bantuan'])
-def bantuan(message):
-    bot.reply_to(message, 'Apakah Ada Yang bisa saya bantu? \nPilih menu di bawah ini tuan >///<\n\n ')
-    markup = types.ReplyKeyboardMarkup()
-    item3 = types.KeyboardButton('/menu')
-    item4 = types.KeyboardButton('/start')
-    item5 = types.KeyboardButton('/cek_id')
-    markup.row(item3, item4)
-    markup.row(item5)
+    markup.row(item5, item4)
     chatid = message.chat.id
     bot.send_message(chatid, 'Silahkan pilih', reply_markup=markup)
 
 @bot.message_handler(commands=['cek_id'])
 def action_id(message):
+    
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
     id_telegram = message.from_user.id
@@ -56,31 +61,66 @@ Username = {}
 ID = {}
 '''.format(first_name,last_name, username, id_telegram))    
     
-@bot.message_handler(commands=['menu'])
+@bot.message_handler(commands=['help'])
 def menu(message):
+    
+    bot.reply_to(message, 'Apakah Ada Yang bisa saya bantu? \nPilih menu di bawah ini tuan >///<')
+    markup = types.ReplyKeyboardMarkup()
+    item4 = types.KeyboardButton('/start')
+    item5 = types.KeyboardButton('/cek_id')
+    markup.row(item5, item4)
     bot.reply_to(message, ''' -= TOOLS MENU =-
 
 
+Note: Bila anda memasukkan kalimat yg terdapat tanda " "(spasi) maka, Ubahlah tanda " " (spasi), menjadi tanda "_" (garis bawah/underscore).
+
+contoh: hai saya BOT. di ubah menjadi: hai_saya_BOT
+
+
 Check ID:
-ex = /cek_id
+/cek_id — Digunakan untuk mengecek ID Telegram anda, cara penggunaan nya.
+ex = /cek_id 
+
+Tanya Wiki:
+/apa_itu — Digunakan untuk mencaritahu sesuatu, dan mendapatkan jawaban dari wikipeadia, contoh.
+ex = /apa_itu cahaya
+ex2 = /apa_itu Google_LLC (gunakan tanda _ untuk pengganti tanda spasi)
+
+YT Audio Download:
+/yt_audio — Digunakan untuk download musik youtube, cara penggunaan.
+ex = /yt_audio https://youtube/gcJVgFQwnpc
+
+YT Audio Download v2:
+/yt_audio2 — ini adalah versi ke2 yt downloader, dapat mendownload secara otomatis, cara penggunaan.
+ex = /yt_audio2 https://youtube/gcJVgFQwnpc
 
 Domain To IP:
-ex = /domain_to_ip website.com
+/domain_to_ip — Digunakan mengubah dari domain ke IP, cara penggunaan nya.
+ex = /domain_to_ip example.com
 
 MD5:
-ex = /md5 string
+/md5 — Digunakan untuk mengubah kalimat / huruf ke hash MD5, cara penggunaan nya.
+ex = /md5 example
 
 SHA1:
-ex = /sha1 string
+/sha1 — Digunakan untuk mengubah kalimat / huruf ke hash sha1, cara penggunaan nya.
+ex = /sha1 example
 
 IP Geolocation:
-ex = /ip_geo 8.8.8.8 (your ip)
+/ip_geo — Digunakan untuk mengetahui informasi tentang IP, mulai dari nama negara hingga ke lokasi IP tersebut, cara penggunaan nya.
+ex = /ip_geo 8.8.8.8
 
 Password Hash:
-ex = /pw_hash yourpw
+/pw_hash — Digunakan untuk mengencripsi password ke hash md5 hingga hash sha512, cara penggunaan nya.
+ex = /pw_hash yourPass123
 
 Random Quotes:
-ex = /random_quotes''')
+/random_quotes — Digunakan untuk memunculkan kata kata keren dan memotivasi, cara penggunaan nya.
+ex = /random_quotes
+
+ClickJacking Tester
+/cj_tester — Digunakan untuk mengecek sebuah website apakah website tersebut vuln terhadap clickjacking atau tidak, cara penggunaan nya.
+ex = /cj_tester https://example.com''')
 
 
 # BAGIAN TOOLS
@@ -89,9 +129,10 @@ ex = /random_quotes''')
     
 @bot.message_handler(commands=['domain_to_ip'])
 def domain_to_ip(message):
-    bot.reply_to(message, "Tutorial : \n\nEx = /domain_to_ip google.com")
+    
     texts = message.text.split()
-    text = texts[1]
+    filter = texts[1]
+    text = filter.replace("_", " ")
     
     if text:
         bot.reply_to(message, "Cek Domain {}".format(text))
@@ -99,24 +140,32 @@ def domain_to_ip(message):
         
 @bot.message_handler(commands=['md5'])
 def hashmd5(message):
+    
     texts = message.text.split()
-    text = texts[1]
+    filter = texts[1]
+    text = filter.replace("_", " ")
     md5 = hashlib.md5()
     md5.update(text.encode("utf-8"))
-    bot.reply_to(message, " nilai hash md5 : {}".format(md5.hexdigest()))
+    bot.reply_to(message, '''nilai asli : {} 
+nilai hash md5 : {}'''.format(text, md5.hexdigest()))
 
 @bot.message_handler(commands=['sha1'])
 def hashsha(message):
+    
     texts = message.text.split()
-    text = texts[1]
+    filter = texts[1]
+    text = filter.replace("_", " ")
     sha = hashlib.sha1()
     sha.update(text.encode("utf-8"))
-    bot.reply_to(message, " nilai hash sha1 : {}".format(sha.hexdigest()))
+    bot.reply_to(message, '''nilai asli : {} 
+nilai hash sha1 : {}'''.format(text, sha.hexdigest()))
         
 @bot.message_handler(commands=['ip_geo'])
 def ipgeo(message):
+    
     texts = message.text.split()
-    ipaddr = texts[1]
+    filter = texts[1]
+    ipaddr = filter.replace("_", " ")
     ipreq = requests.get(f"http://ip-api.com/json/{ipaddr}")
 
     if ipreq.status_code == 200:
@@ -134,6 +183,7 @@ def ipgeo(message):
         
 @bot.message_handler(commands=['pw_hash'])
 def pw_hash(message):
+    
     texts = message.text.split()
     text = texts[1]
     sha1 = hashlib.sha1()
@@ -147,7 +197,9 @@ def pw_hash(message):
     sha256.update(text.encode("utf-8"))
     sha384.update(text.encode("utf-8"))
     sha512.update(text.encode("utf-8"))
-    bot.reply_to(message, f'''Password hash sha1 :  {sha1.hexdigest()}
+    bot.reply_to(message, f'''Note: Password tidak boleh menggunakan spasi ( )
+
+Password hash sha1 :  {sha1.hexdigest()}
 
 Password hash md5 :  {md5.hexdigest()}
 
@@ -159,11 +211,158 @@ Password hash sha512 :  {sha512.hexdigest()}''')
         
 @bot.message_handler(commands=['random_quotes'])
 def random_quotes(message):
+    
     file = open('q.json','r').read()
     sl = json.loads(file)
     js = random.choice(sl)
     bot.reply_to(message, f'{js["quote"]} \n\nby {js["nama"]}')
         
+@bot.message_handler(commands=['cj_tester'])
+def ClickjackingTester(message):
+    
+    def check(url):
+
+        try:
+            if "http" not in url: url = "http://" + url
+
+            data = urlopen(url)
+            headers = data.info()
+
+            if not "X-Frame-Options" in headers: return True
+
+        except: return False
+
+
+    def code_html(url):
+
+        code = f"""
+<html>
+    <head><title>Clickjack test page</title></head>
+    <body>
+        <p>Website is vulnerable to clickjacking!</p>
+        <iframe src="{url}" width="500" height="500" ></iframe>
+    </body>
+</html>
+        """
+        return code
+
+    texts = message.text.split()
+    site = texts[1]
+
+        
+    bot.reply_to(message, f"[*] Checking {site}")
+    status = check(site)
+
+    if status:
+        bot.reply_to(message, f" [+] Website is vulnerable! \n[*] Copy This Code and Try This \n{code_html(site)}")
+    elif not status: 
+        bot.reply_to(message, " [-] Website is not vulnerable!")
+    else: 
+        bot.reply_to(message, "ERROR")
+        
+@bot.message_handler(commands=['apa_itu'])
+def wiki(message):
+    
+    texts = message.text.split()
+    filter = texts[1]
+    text = filter.replace("_", " ")
+    if text:
+        try:
+            dpt = f'https://id.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles={text}'
+            hasil = requests.get(dpt).json()
+            filter = hasil['query']['pages']
+            d = re.findall(r'(\d+)', str(filter))
+            result = filter[d[0]]['extract']
+            bot.reply_to(message, f'''Pertanyaan : {text}
+
+Jawaban : {result}''')
+        except Exception as e:
+            print(e)
+            bot.reply_to(message, f'Maaf, Yang anda cari "{text}" tidak bisa ditemukan di wikipedia!')
+            
+@bot.message_handler(commands=['yt_audio'])
+def ytAudio(message):
+    
+    texts = message.text.split()
+    text = texts[1]
+    if text:
+        try:
+            url = text.replace('[','').replace(']','')
+            ytAudio = requests.post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
+            find = bes(ytAudio['result'], 'html.parser').findAll('td')
+            ukuran = find[len(find)-10].text
+            id = re.findall('var k__id = "(.*?)"', ytAudio['result'])
+            judul = bes(ytAudio['result'], 'html.parser').find('b').text
+            link_download = bes(requests.post('https://www.y2mate.com/mates/en60/convert',data={'type':url.split('/')[2],'_id':id[0],'v_id':url.split('/')[3],'ajax':'1','token':'','ftype':'mp3','fquality':'128'}).json()['result'],'html.parser').find('a')['href']
+            bot.reply_to(message, f'''Judul : {judul}
+Ukuran : {ukuran}
+Download link : {link_download}''')
+        except Exception as e:
+            print(e)
+            bot.reply_to(message, "Maaf sepertinya link ini telah error")
+
+@bot.message_handler(commands=['yt_audio2'])
+def ytAudio(message):
+    texts = message.text.split()
+    filter = texts[1]
+    url = pafy.new(filter)
+    bot.reply_to(message, f'''Note : versi k2 dalam proses pengembangan, maaf jika ada error
+
+Judul : {url.title}
+Thumbnail : {url.thumb}
+Durasi : {url.duration}
+
+Sedang Mendownload Mohon Tunggu Sesaat''')
+    result = url.getbestaudio()
+    result.download(f'{url.title}.mp3')
+    
+    for i in os.listdir():
+        if i.endswith(".mp3"):
+            print(i)
+            bot.send_audio(message.chat.id, open(i, 'rb'))
+            os.remove(i)
+            
+# @bot.message_handler(commands=['story_horror'])
+# def story(message):
+    
+#     texts = message.text.split()
+#     filter = texts[1]
+#     text = filter.replace("_", " ")
+#     if text == "judul":
+#         query = "select judul_cerita from cerita"
+#         sql.execute(query)
+#         result = sql.fetchall()
+#         pesan = ''
+#         for data in result:
+#             pesan = pesan + 'Judul : ' + str(data).replace("'", "").replace("(", "").replace(")", "").replace(",", "")  + '\nCara Akses : /story_horror ' + str(data).replace(" ", "_").replace("'", "").replace("(", "").replace(")", "").replace(",", "") + '\n\n'
+#         bot.reply_to(message, pesan)
+#     else:
+#         query = f"select isi_cerita from cerita where judul_cerita = '{text}'"
+#         sql.execute(query)
+#         result = sql.fetchall()
+#         pesan = ''
+#         for data in result:
+#             pesan = pesan + str(data) + '\n'
+#             if len(str(data)) > 4096:
+#                 text = "Cerita Gagal Terkirim"
+#                 pesan = f"Maaf, cerita ini tidak dapat terkitim karena melebihi 4096 karakter/batas karakter chat di telegram. \ncerita ini memiliki {len(str(data))} karakter"
+#         bot.reply_to(message, f'''{text}
+
+# {pesan.replace("'", "").replace("(", "").replace(")", "")}''')
+        
+@bot.message_handler(commands=['add_story'])
+def add_story(message):
+    if message.from_user.id == id and message.from_user.username == username_owner:
+        bot.reply_to(message, "Cerita Anda Segera Kami Proses")
+    else:
+        bot.reply_to(message, "Sorry, just owner can use this commands")
+    
+    
+@bot.message_handler(commands=['tes_inp'])
+def tes_inp(message):
+    text = message.text.replace("/tes_inp ", "")
+    bot.reply_to(message, text)
+    
 while True:
     try:
         bot.polling()
